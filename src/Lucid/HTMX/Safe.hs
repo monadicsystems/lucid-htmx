f326f773888cccb805ccd1ccad788a9b119ab954
Ubuntu <ubuntu@ip-172-31-27-208.us-west-2.compute.internal>
2021-09-09 09:43:10 +0000

    Fill in blanks. Add more types.

--- src/Lucid/HTMX/Safe.hs
+++ src/Lucid/HTMX/Safe.hs
@@ -79,3 +79,104 @@ hx_ext_ htmxExts = Base.hx_ext_ $ case htmxExts of
             [] -> ""
             htmxExtList -> Text.intercalate "," $ Prelude.map (Text.pack . show) htmxExtList
 
+hx_get_ :: Link -> Text
+hx_get_ = undefined
+
+-- Placeholder until I can bring in aeson...
+type JSON = Text
+
+class ToJSON a where
+    toJSON :: a -> JSON
+
+class FromJSON a where
+    fromJSON :: JSON -> a
+
+newtype JavaScript = JavaScript { unJavaScript :: Text } deriving (Eq, Show)
+
+hx_headers_ :: ToJSON a => a -> Attribute
+hx_headers_ = undefined
+
+hx_history_elt_ :: Attribute
+hx_history_elt_ = Base.hx_history_elt_
+
+data QuerySelector =
+    QuerySelectorTag Text
+    | QuerySelectorDot Text
+    | QuerySelectorHash Text
+    | QuerySelectorAttribute Text Text
+    deriving (Eq, Show)
+
+hx_include_ :: QuerySelector -> Attribute
+hx_include_ = undefined
+
+data HXIndicatorSelector = 
+    HXIndicatorSelector QuerySelector
+    | HXIndicatorSelectorClosest QuerySelector
+
+hx_indicator_ :: HXIndicatorSelector -> Attribute
+hx_indicator_ = undefined
+
+data HXParams =
+    HXParamsAll
+    | HXParamsNone
+    | HXParamsNotList (Set Text)
+    | HXParamsList (Set Text)
+    deriving (Eq, Show)
+
+hx_params_ :: HXParams -> Attribute
+hx_params_ = undefined
+
+hx_patch_ :: Link -> Attribute
+hx_patch_ = undefined
+
+hx_post_ :: Link -> Attribute
+hx_post_ = undefined
+
+-- For same reasons as hx_boost_
+hx_preserve_ :: Attribute
+hx_preserve_ = Base.hx_preserve_ "true"
+
+hx_prompt_ :: Text -> Attribute
+hx_prompt_ = Base.hx_prompt_
+
+hx_push_url_ :: Link -> Attribute
+hx_push_url_ = undefined
+
+hx_put_ :: Link -> Attribute
+hx_put_ = undefined
+
+-- Still needs more research below
+type HXRequest = Text
+
+hx_request_ :: HXRequest -> Attribute
+hx_request_ = undefined
+
+hx_select_ :: QuerySelector -> Attribute
+hx_select_ = undefined
+
+-- More research
+type HXSSE = Text
+
+hx_sse_ :: HXSSE -> Attribute
+hx_sse_ = undefined
+
+type HXSwapOOB = Text
+
+hx_swap_oob_ :: HXSwapOOB -> Attribute
+hx_swap_oob_ = undefined
+
+data HXSwap =
+    HXSwapInner
+    | HXSwapOuter
+    | HXSwapBeforeBegin
+    | HXSwapAfterBegin
+    | HXSwapBeforeEnd
+    | HXSwapAfterEnd
+    | HXSwapNone
+    deriving (Eq, Show)
+
+hx_swap_ :: HXSwap -> Attribute
+hx_swap_ = undefined
+
+-- More research
+
